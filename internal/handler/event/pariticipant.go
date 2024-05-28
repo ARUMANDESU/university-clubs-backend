17a62188d90ba441c3416a2e79f4e6619a55ec18
ARUMANDESU <zh.arumandes@gmail.com>
2024-05-29 01:25:19 +0500

    kick participant

--- internal/handler/event/pariticipant.go
+++ internal/handler/event/pariticipant.go
@@ -2,6 +2,7 @@ package event
 
 import (
 	eventv1 "github.com/ARUMANDESU/uniclubs-protos/gen/go/posts/event"
+	"github.com/ARUMANDESU/university-clubs-backend/internal/handler/utils"
 	"github.com/gin-gonic/gin"
 	"log/slog"
 	"net/http"
@@ -56,6 +57,42 @@ func (h *Handler) LeaveEventHandler(c *gin.Context) {
 	_, err := h.eventClient.CancelParticipation(c, &eventv1.EventActionRequest{
 		EventId: eventID,
 		UserId:  userID,
+	})
+	if err != nil {
+		h.handleEventError(c, log, err)
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
+
+func (h *Handler) RemoveParticipantHandler(c *gin.Context) {
+	const op = "event.Handler.RemoveParticipantHandler"
+	log := h.log.With(slog.String("op", op))
+
+	eventID := c.Params.ByName("id")
+	if eventID == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "event_id parameter must be provided"})
+		return
+	}
+
+	participantId, err := utils.GetIntFromParams(c.Params, "participant_id")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userIDFromCtx, ok := c.Get("userID")
+	if !ok {
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+	userID := userIDFromCtx.(int64)
+
+	_, err = h.eventClient.KickParticipant(c, &eventv1.KickParticipantRequest{
+		EventId:       eventID,
+		UserId:        userID,
+		ParticipantId: participantId,
 	})
 	if err != nil {
 		h.handleEventError(c, log, err)
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -180,7 +180,7 @@ func (h *Handler) InitRoutes() *gin.Engine {
 
 			eventPathAuth.POST("/:id/participants", h.EventHandler.AddParticipantHandler)
 			eventPathAuth.DELETE("/:id/participants", h.EventHandler.LeaveEventHandler)
-			//eventPathAuth.DELETE("/:id/participants/:participant_id:", h.EventHandler.RemoveParticipantHandler)
+			eventPathAuth.DELETE("/:id/participants/:participant_id", h.EventHandler.RemoveParticipantHandler)
 			//eventPathAuth.POST("/:id/participants/:participant_id/ban", h.EventHandler.BanParticipantHandler)
 
 			eventPathAuthAdmin := eventPathAuth.Group("")
